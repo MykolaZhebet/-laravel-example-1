d76c648f87c40cb045950b2e8bffd27d95909bf1
Mykola Zhebet <phpapple@gmail.com>
2025-11-03 10:37:05 +0200

    init with custom view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+<h1>Testing 1</h1>
+<div>
+    <h2>Register</h2>
+    <form action="">
+        <input type="text" name="name" placeholder="Name">
+        <input type="text" name="email" placeholder="Email">
+        <input type="text" name="password" placeholder="">
+        <input type="submit" name="submit" value="Register">
+    </form>
+</div>
+</body>
+</html>
