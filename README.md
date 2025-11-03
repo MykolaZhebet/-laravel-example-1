ab5eb6645795e94e6990043785e95941b699ca6a
Mykola Zhebet <phpapple@gmail.com>
2025-11-03 14:14:27 +0200

    feat: Simple authentication logic

--- README.md
+++ README.md
@@ -59,3 +59,11 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
 
 
+## Useful command:
+```
+php artisan serve
+php artisan migrate
+
+php artisan make:controller UserController
+
+```
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,44 @@
 </head>
 <body>
 <h1>Testing 1</h1>
-<div>
+@auth
+<p>You are logged in</p>
+    <form action="/logout" method="POST">
+        @csrf
+        <button>Logout</button>
+    </form>
+@else
+    <div>
     <h2>Register</h2>
-    <form action="">
+    <form action="/register" method="POST">
+        @csrf
         <input type="text" name="name" placeholder="Name">
         <input type="text" name="email" placeholder="Email">
-        <input type="text" name="password" placeholder="">
-        <input type="submit" name="submit" value="Register">
+        <input type="password" name="password" placeholder="password">
+{{--        <input type="submit" name="submit" value="Register">--}}
+        <button>Register</button>
     </form>
-</div>
+    <h2>Login</h2>
+    <form action="/login" method="POST">
+        @csrf
+        <input type="text" name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="password">
+{{--        <input type="submit" name="submit" value="Register">--}}
+        <button>Login</button>
+    </form>
+    </div>
+@endauth
+
+
+@if ($errors->any())
+    <div style="color: red; margin-bottom: 10px;">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 </body>
 </html>
