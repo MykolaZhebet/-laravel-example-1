f1680fb7d3a6d149bfada39fb727197b5290d820
Mykola Zhebet <phpapple@gmail.com>
2025-11-04 13:17:25 +0200

    feat(auth): init auth logic with laravel breeze for authentication logic(Blade with Alpine.js )

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,24 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
-use App\Models\Post;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 
 Route::get('/', function () {
     //        return view('welcome');
@@ -15,12 +30,14 @@ Route::get('/', function () {
     return view('home', ['posts' => $posts]);
 });
 
-Route::post('/register', [UserController::class, 'register']);
-
-Route::post('/logout', [UserController::class, 'logout']);
-Route::post('/login', [UserController::class, 'login']);
+// Route::post('/register', [UserController::class, 'register']);
+// Route::post('/logout', [UserController::class, 'logout']);
+// Route::post('/login', [UserController::class, 'login']);
 
 Route::post('/create-post', [PostController::class, 'createPost']);
 Route::get('/edit-post/{post}', [PostController::class, 'showEditPost']);
 Route::put('/edit-post/{post}', [PostController::class, 'editPost']);
 Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
+
+
+require __DIR__ . '/auth.php';
