e15c98b7d59694a5cc71bcfd5d3f6d9280d10bd2
Mykola Zhebet <phpapple@gmail.com>
2025-11-04 18:58:50 +0200

    move categories in separate class based component

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,10 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        $categories = Category::get();
         $posts = Post::orderBy('created_at', 'DESC')->paginate(5);
         return view('dashboard', [
-            'categories' => $categories,
             'posts' => $posts
         ]);
     }
