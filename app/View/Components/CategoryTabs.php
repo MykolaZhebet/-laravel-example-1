73d9c2714376ef10ef6ec2483ff77ffd70ebd6b2
Mykola Zhebet <phpapple@gmail.com>
2025-11-04 19:02:26 +0200

    provide simple text to the compoent (without variable binding: :name)

--- app/View/Components/CategoryTabs.php
+++ app/View/Components/CategoryTabs.php
@@ -12,7 +12,7 @@ class CategoryTabs extends Component
     /**
      * Create a new component instance.
      */
-    public function __construct() {}
+    public function __construct(public string $name) {}
 
     /**
      * Get the view / contents that represent the component.
