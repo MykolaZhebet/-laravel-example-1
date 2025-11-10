a8ed2df28f8a66e7809938c57a1dfeb56ddc813a
Mykola Zhebet <phpapple@gmail.com>
2025-11-10 11:02:06 +0200

    work with static assets(image)

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import.meta.glob(['../images/**']);
 window.Alpine = Alpine;
 
 Alpine.start();
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        {{-- <x-application-logo class="block h-9 w-auto fill-current text-gray-800" /> --}}
+                        <img width="50" src="{{ Vite::asset('resources/images/custom_logo.jpeg') }}" alt="Site log" />
                     </a>
                 </div>
 
@@ -59,7 +60,7 @@
 
                                     <x-dropdown-link :href="route('logout')"
                                         onclick="event.preventDefault();
-                                                                                            this.closest('form').submit();">
+                                                                                                                            this.closest('form').submit();">
                                         {{ __('Log Out') }}
                                     </x-dropdown-link>
                                 </form>
@@ -120,7 +121,7 @@
 
                         <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
-                                                                                            this.closest('form').submit();">
+                                                                                                                            this.closest('form').submit();">
                             {{ __('Log Out') }}
                         </x-responsive-nav-link>
                     </form>
