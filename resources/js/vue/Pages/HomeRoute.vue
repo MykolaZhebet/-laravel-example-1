6cf37d601b3a9612f7991e34361a0847bae168af
Mykola Zhebet <phpapple@gmail.com>
2025-11-11 20:26:23 +0200

    async request from vue to BE with axios

--- resources/js/vue/Pages/HomeRoute.vue
+++ resources/js/vue/Pages/HomeRoute.vue
@@ -1,6 +1,25 @@
+<script setup>
+import { ref } from "vue";
+import axios from 'axios';
+//API call Trigger by button
+const responseRef = ref('default value');
+
+const triggerEndpoint = async () => { 
+    try {
+        let response = await axios.get('/api/hello');
+        responseRef.value = response.data.message;
+    } catch (error) { 
+        console.log(error);
+    }
+
+}
+//API call Trigger by button
+</script>
 <template>
     <div>
         <h2>Home page</h2>
         <router-link to="/vue/test">Test page link</router-link>
+        <button @click.prevent="triggerEndpoint">Trigger BE API request</button>
+        <p v-if="responseRef">Response will be here: {{ responseRef }}</p>
     </div>
 </template>
